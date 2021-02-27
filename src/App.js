232f691a4ebba96361652ec6878325a78dfbe462
Keigo Takahashi <tkhskig29@gmail.com>
2021-02-27 20:30:24 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
+//class App extends Component {
+//  render() {
+//    return <div>Hello, world!</div>;
+//  }
+//}
+
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>;
+    return React.createElement(
+      "h1",
+      null,
+      "Hello,Wold!!"
+    )
   }
 }
 
